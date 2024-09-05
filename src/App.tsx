6e4784f8a9509c6f90927a6a8f88b8ae95285446
Pedro Barros <pedrobars7a@gmail.com>
2024-09-05 11:24:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello, Vite + TypeScript + TailwindCSS!
+      </h1>
+    </div>
+  )
+}
+
+export default App
