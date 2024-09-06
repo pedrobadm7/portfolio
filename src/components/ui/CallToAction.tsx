2aab1223198ee84fac8cec0f33e403fed9a882ce
Pedro Barros <pedrobars7a@gmail.com>
2024-09-06 20:58:03 -0300

    feat: create call to action to whatsapp

--- src/components/ui/CallToAction.tsx
+++ src/components/ui/CallToAction.tsx
@@ -1,6 +1,10 @@
-import { PhoneIcon } from '../icons/PhoneIcon'
+import { PhoneIcon } from '../icons/PhoneIcon';
 
 export const CallToAction = () => {
+  const phoneNumber = "73999577204";
+  const message = "Olá! Gostaria de conversar sobre uma possível colaboração.";
+  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
   return (
     <section id="cta" className="w-full py-12 md:py-24 lg:py-32 bg-muted flex items-center justify-center">
       <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
@@ -12,7 +16,8 @@ export const CallToAction = () => {
           </p>
         </div>
         <div> <a
-          href="#"
+          href={whatsappLink}
+          target='_blank'
           className="inline-flex items-center justify-center rounded-md bg-primary px-8 py-3 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus:bg-primary focus:text-primary-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50"
         >
           <PhoneIcon className="mr-2 h-5 w-5" />
